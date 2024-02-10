a3d5d715bdcc33b7a16db10ddc41c2e9fbcef124
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-02-10 12:24:20 +0530

    modified

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.main,name='main'),
-    path('news/',views.news,name='news'),
-    path('contact/',views.contact,name='contact')
+    path('register/',views.user_reg,name='register'),
+    path('login/',views.login,name='login'),
+    path('userlogin/',views.user_login,name='userlogin'),
 ]
--- travelapp/views.py
+++ travelapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Team,Places
+from django.contrib.auth.models import User
 # Create your views here.
 
 def main(request):
@@ -7,8 +8,40 @@ def main(request):
 	places=Places.objects.all()
 	return render(request,'index.html',{'team':team,'places':places})
 
-def news(request):
-	return render(request,'news.html')
+def user_reg(request):
+	if request.method=='POST':
+		name=request.POST.get('name')
+		password=request.POST.get('pass')
+		check_pass=request.POST.get('check_pass')
+		if password==check_pass:
+			user=User(username=name,password=password)
+			user.save()
+			return redirect('login')
+	return render(request,'register.html')
 
-def contact(request):
-	return render(request,'contact.html')
+def login(request):
+	return render(request,'login.html')
+
+from django.shortcuts import render, redirect
+from .models import Team, Places
+from django.contrib.auth.models import User
+
+def user_login(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('pass')
+
+        try:
+        	team=Team.objects.all()
+        	places=Places.objects.all()
+        	user=User.objects.get(username=name)
+        	if user.password == password and user.username == name:
+        		return render(request, 'index.html', {'name': name,'team':team,'places':places})
+        	else:
+        		return render(request, 'login.html')
+        except User.DoesNotExist:
+        	return render(request, 'login.html')
+    else:
+    	return render(request, 'login.html')
+
+
